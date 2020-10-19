a0572941a479141d7473294f9a751ed431258225
vahab ghadiri <>
2020-10-20 00:48:25 +0330

    `InAppUpdateReceiver` added

---
+++ in-app-update/src/main/java/com/farsitel/bazaar/inappupdate/receiver/InAppUpdateReceiver.kt
@@ -0,0 +1,12 @@
+package com.farsitel.bazaar.inappupdate.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class InAppUpdateReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        // todo not implemented
+    }
+}
