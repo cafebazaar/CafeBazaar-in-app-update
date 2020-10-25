7691e5a9a36f7dd6221f783cc642e1757bae847c
vahab ghadiri <>
2020-10-25 19:06:33 +0330

    AppUpdateInfo.kt methods added, so object is now aware of install state and other update info

--- in-app-update/src/main/java/com/farsitel/bazaar/inappupdate/model/AppUpdateInfo.kt
+++ in-app-update/src/main/java/com/farsitel/bazaar/inappupdate/model/AppUpdateInfo.kt
@@ -1,4 +1,37 @@
 package com.farsitel.bazaar.inappupdate.model
 
+import com.farsitel.bazaar.inappupdate.install.InstallStatus
+
 class AppUpdateInfo {
+
+    private var installStatus: InstallStatus = InstallStatus.UNKNOWN
+    private var versionCode: Int = -1
+
+    fun setAvailableVersionCode(versionCode: Int) {
+        this.versionCode = versionCode
+    }
+
+    fun setInstallStatus(installStatus: InstallStatus) {
+        this.installStatus = installStatus
+    }
+
+    fun installStatus(): InstallStatus = installStatus
+
+    fun bytesDownloaded(): Long {
+        TODO("Not yet implemented")
+    }
+
+    fun totalBytesToDownload(): Long {
+        TODO("Not yet implemented")
+    }
+
+    fun availableVersionCode(): Int = versionCode
+
+    fun isUpdateTypeAllowed(updateType: UpdateType): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    fun updateAvailability(): Int {
+        TODO("Not yet implemented")
+    }
 }
